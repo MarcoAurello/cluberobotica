dfc9f6912e4838a0f1ee62b21d61a39bd969b340
Marco Aurellio Machado Nunes <marco.nunes@hemobras.gov.br>
2026-02-02 15:05:44 -0300

    site

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ window.onclick = function(e) {
   const modal = document.getElementById("modalImagem");
   if (e.target === modal) fecharImagem();
 }
+
+let currentSlide = 0;
+
+const track = document.querySelector('.carousel-track');
+const slides = document.querySelectorAll('.carousel-track img');
+
+function updateCarousel() {
+  track.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  updateCarousel();
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  updateCarousel();
+}
+
